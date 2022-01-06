5ec29f9bc5a955976b9dca008245f64037121402
Adriansyah7 <64959841+Adriansyah7@users.noreply.github.com>
2022-01-06 08:29:46 +0700

    Add files via upload

--- galeri.php
+++ galeri.php
@@ -7,16 +7,27 @@ if (!isset($_SESSION["login"])) {
 }
 require 'function.php';
 
-$foto = query("SELECT * FROM galeri ORDER BY id DESC");
+    // Pagination
+    // konfigurasi
+    $jumlahPerPage = 6;
+    $jumlahData = count(query("SELECT * FROM galeri"));
+    $jumlahHalaman = ceil($jumlahData / $jumlahPerPage);
+    $pageAktif = (isset($_GET["page"])) ? $_GET["page"] : 1;
+    $awalData = ($jumlahPerPage * $pageAktif) - $jumlahPerPage;
 
-// Tombol Cari di tekan
-if (isset($_POST["cari"])) {
-    $foto = cari($_POST["keyword"]);
-}
+
+    $foto = query("SELECT * FROM galeri ORDER BY id DESC LIMIT $awalData, $jumlahPerPage");
+
+
+    // Tombol Cari di tekan
+    if (isset($_POST["cari"])) {
+        $foto = cari($_POST["keyword"]);
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,65 +46,94 @@ if (isset($_POST["cari"])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 
 </head>
+
 <body>
 
 
 
 
-<!-- Navbar -->
-<nav>
-    <div class="container">
+    <!-- Navbar -->
+    <nav>
+        <div class="container">
 
-        <h4 class="logo">
-            <a class="link" href="index.php"> Beranda </a>
-            <a class="link" href="Upload.php"> Upload </a>
-        </h4>
+            <h4 class="logo">
+                <a class="link" href="index.php"> Beranda </a>
+                <a class="link" href="Upload.php"> Upload </a>
+            </h4>
 
-        <div class="search-bar">
-            <form action="" method="post">
-                <i class="uil uil-search-alt"></i>
-                <input type="search" name="keyword" placeholder="Cari disini" autocomplete="off" id="keyword">
-                <button class="btn btn-primary" type="submit" name="cari" id="tombol-cari">Cari</button>
-            </form>
+            <div class="search-bar">
+                <form action="" method="post">
+                    <i class="uil uil-search-alt"></i>
+                    <input type="search" name="keyword" placeholder="Cari disini" autocomplete="off" id="keyword">
+                    <button class="btn btn-primary" type="submit" name="cari" id="tombol-cari">Cari</button>
+                </form>
+            </div>
+
+            <h4 class="logo">
+                <a class="links">------ </a>
+                <a class="link" href="galeri.php"> Kembali </a>
+            </h4>
+        </div>
+    </nav>
+
+    <br>
+    <br>
+    <br>
+
+
+    <!-- Galeri -->
+    <div id="container">
+        <div class="gallery">
+
+            <div class="image-container">
+                <?php foreach ($foto as $row) : ?>
+                    <a href="img/<?= $row["gambar"]; ?>" class="image" title="<?= $row["subjek"]; ?> <?= $row["detail"]; ?> <?= $row["wilayah"]; ?> <?= $row["tahun"]; ?>">
+                        <img src="img/<?= $row["gambar"]; ?>" alt="">
+                    </a>
+                <?php endforeach; ?>
+
+            </div>
+
         </div>
         
-        <h4 class="logo">
-            <a class="links">------ </a>
-            <a class="link" href="galeri.php"> Kembali </a>
-        </h4>
+        <center>
+            <section class="pagination">
+                <ul class="pagination-items">
+                    <?php if ($pageAktif != 1) : ?>
+                        <a href="?page=1">FIRST</a>
+                    <?php endif; ?>
+                    <?php if ($pageAktif > 1) : ?>
+                        <a href="?page=<?= $pageAktif - 1; ?>">&laquo;</a>
+                    <?php endif; ?>
+
+                    <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+                        <?php if ($i == $pageAktif) : ?>
+                            <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+                        <?php else : ?>
+                            <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+                        <?php endif; ?>
+                    <?php endfor; ?>
+                    <?php if ($pageAktif < $jumlahHalaman) : ?>
+                        <a href="?page=<?= $pageAktif + 1; ?>">&raquo;</a>
+                    <?php endif; ?>
+                    <?php if ($pageAktif != $jumlahHalaman) : ?>
+                        <a href="?page= <?= $jumlahHalaman ?>">LAST</a>
+                    <?php endif; ?>
+                </ul>
+            </section>
+        </center>
+
+
+
+        <footer>
+            <div align="center">
+                <p>Dokumentasi Kinerja Kegiatan Kantor <br> ~~* IPDS3273 @2021 *~~</p>
+                <h6><a href="https://www.creative-tim.com" target="_blank">©2021 </a>.</h6>
+            </div>
+        </footer> <br>
+
     </div>
-</nav>
-
-<br>
-<br>
-<br>
-
-
-<!-- Galeri -->
-<div id="container">
-    <div class="gallery">
-
-        <div class="image-container">
-            <?php foreach ($foto as $row) : ?>
-                <a href="img/<?= $row["gambar"]; ?>" class="image" title="<?= $row["subjek"]; ?> <?= $row["detail"]; ?> <?= $row["wilayah"]; ?> <?= $row["tahun"]; ?>">
-                    <img src="img/<?= $row["gambar"]; ?>" alt="">
-                </a>
-            <?php endforeach; ?>
-
-        </div>
-
-    </div>
-
-
-
-<footer>
-    <div align="center">
-        <p>Dokumentasi Kinerja Kegiatan Kantor <br> ~~* IPDS3273 @2021 *~~</p>
-        <h6><a href="https://www.creative-tim.com" target="_blank">©2021 </a>.</h6>
-    </div>
-</footer>  <br>
-
-</div>
-<script src="galeri.js"> </script> 
+    <script src="galeri.js"> </script>
 </body>
+
 </html>
